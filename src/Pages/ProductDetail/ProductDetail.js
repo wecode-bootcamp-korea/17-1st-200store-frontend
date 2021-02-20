fdae924a6080cf1a080064e63fd46b660ef5f50d
peng0code <peng0code>
2021-02-20 20:03:23 +0900

    ADD: 상품상세정보 컴포넌트화 완료

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -4,6 +4,7 @@ import './ProductDetail.scss';
 import Reviews from './review';
 import AdminMsg from './admin_msg';
 import Itemgoodstab from './Itemgoodstab';
+import ProductdetailTable from './productdetailtable';
 import Modal from './Modal';
 
 class ProductDetail extends React.Component {
@@ -210,52 +211,7 @@ class ProductDetail extends React.Component {
                     <img src={image.imageURL} alt="사진" />
                     <div className="detailInfo">
                       <h3 className="mustInfo">상품필수정보</h3>
-                      <table className="leftTableType">
-                        <tbody>
-                          <tr>
-                            <th>품명</th>
-                            <td colspan="3">을지로에서 만든 쟁반</td>
-                          </tr>
-                          <tr>
-                            <th>재질</th>
-                            <td colspan="3">스테인레스</td>
-                          </tr>
-                          <tr>
-                            <th>구성품</th>
-                            <td colspan="3">쟁반, 품질보증서</td>
-                          </tr>
-                          <tr>
-                            <th>크기</th>
-                            <td colspan="3">32.5x32.5cm</td>
-                          </tr>
-                          <tr>
-                            <th>제조사</th>
-                            <td colspan="3">(주)우아한형제들</td>
-                          </tr>
-                          <tr>
-                            <th>제조국</th>
-                            <td colspan="3">한국</td>
-                          </tr>
-                          <tr>
-                            <th>식품위생법에 따른 수입신고</th>
-                            <td colspan="3">해당없음</td>
-                          </tr>
-                          <tr>
-                            <th>품명</th>
-                            <td colspan="3">을지로에서 만든 쟁반</td>
-                          </tr>
-                          <tr>
-                            <th>품질보증기준</th>
-                            <td colspan="3">
-                              관련 법 및 소비자 분쟁해결 규정에 따름
-                            </td>
-                          </tr>
-                          <tr>
-                            <th>A/S 책임자와 전화번호</th>
-                            <td colspan="3">배민문방구 고객센터 1670-9902</td>
-                          </tr>
-                        </tbody>
-                      </table>
+                      <ProductdetailTable />
                     </div>
 
                     <Itemgoodstab />
