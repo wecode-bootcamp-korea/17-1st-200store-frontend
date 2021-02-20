b8b115cf30a0706998dbd5d276bb7372edc8b2c2
YooJinSuh <rylie.suh35@gmail.com>
2021-02-20 20:07:41 +0900

    Add: 주문 내역 UI 구현중

---
+++ src/Pages/MyPage/Components/OrderHistory/OrderHistory.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './OrderHistory.scss';
+
+class OrderHistory extends Component {
+  render() {
+    return <div className="OrderHistory"></div>;
+  }
+}
+
+export default OrderHistory;
